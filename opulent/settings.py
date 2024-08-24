5b67803172a35b4366d6e46dcf2ecb1e7c3d7c63
unknown <darkonex>
2024-08-24 23:59:04 +0100

    website finished

--- opulent/settings.py
+++ opulent/settings.py
@@ -136,15 +136,13 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'public/staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'public/static',BASE_DIR / "customTheme/static"]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'public/staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'public/static']
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
